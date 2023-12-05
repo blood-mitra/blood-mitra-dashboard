8bc4a409ab73673b573efc02d7a706495beb2e61
Nirwan Rai <nirwanrai6969@gmail.com>
2023-12-05 10:54:41 +0545

    👌 Review: removed excessive loadingOverlay and useDebounced state

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -30,7 +30,7 @@ export const Users = () => {
 
   const [searchTerm, setSearchTerm] = useDebouncedState("", 300);
 
-  const [bloodType, setBloodType] = useDebouncedState("", 300);
+  const [bloodType, setBloodType] = useState("");
 
   const [page, setPage] = useInputState(1);
 
@@ -48,7 +48,7 @@ export const Users = () => {
     location: "",
     order: reverseSortDirection ? "desc" : "asc",
     skip: (page - 1) * rowsPerPage,
-    searchTerm: searchTerm,
+    searchTerm,
   });
 
   const rows = data?.data.data.map((element) => (
@@ -90,7 +90,6 @@ export const Users = () => {
         Users
       </Title>
       <Grid mt={24} mb="md">
-        <LoadingOverlay visible={isLoading} />
         <Grid.Col span={10}>
           <TextInput
             mt={24}
